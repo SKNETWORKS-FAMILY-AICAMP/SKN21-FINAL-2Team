d1a21fc89e5fe3495461b320cf816ab1172804c7
lucy <lucymoon.d@gmail.com>
2026-02-25 20:24:34 +0900

    - 장소 데이터에 llm_text 추가 (Tavily Search 없는 버전)

--- backend/app/scripts/preprocess_data.py
+++ backend/app/scripts/preprocess_data.py
@@ -11,6 +11,8 @@ load_dotenv()
 import base64
 from app.utils.geocoder import GeoCoder
 
+NONE_VALUES = [None, "", [], {}, 0, 0.0]
+
 # download image from URL or Base64
 def download_image(url: str, timeout: int = 10) -> Image.Image | None:
     try:
@@ -52,19 +54,19 @@ def ingest_data(data):
     for item in data:
         def remove_empty_values(data):
             """
-            재귀적으로 dict와 list 내부의 빈 값(None, "", [], {})을 제거합니다.
+            재귀적으로 dict와 list 내부의 빈 값(None, "", [], {}, 0, 0.0)을 제거합니다.
             """
             if isinstance(data, dict):
                 # 딕셔너리 컴프리헨션을 사용해 재귀적으로 탐색
                 return {
                     k: v for k, v in ((k, remove_empty_values(v)) for k, v in data.items())
-                    if v not in (None, "", [], {}, 0)  # 비어있다고 판단할 기준들
+                    if v not in NONE_VALUES
                 }
             elif isinstance(data, list):
                 # 리스트 내부 요소들도 재귀적으로 탐색
                 return [
                     v for v in (remove_empty_values(i) for i in data)
-                    if v not in (None, "", [], {}, 0)
+                    if v not in NONE_VALUES
                 ]
             else:
                 # 더 이상 쪼갤 수 없는 값(str, int, bool 등)은 그대로 반환
@@ -78,7 +80,7 @@ def ingest_data(data):
         address = item.get("addr", "")
         if len(address) > 0:
             result = GeoCoder().eocoder(address)
-            if not result:
+            if result:
                 new_payload['road_address'] = result['road_address']
                 new_payload['old_address'] = result['jibun_address']
                 if lat == 0.0 or lng == 0.0:
@@ -89,8 +91,8 @@ def ingest_data(data):
             if lat != 0.0 and lng != 0.0:
                 latlng = GeoCoder().reverse_geocoder(lat, lng)
                 if latlng is not None:
-                    new_payload['road_address'] = result['road_address']
-                    new_payload['old_address'] = result['jibun_address']
+                    new_payload['road_address'] = latlng['road_address']
+                    new_payload['old_address'] = latlng['jibun_address']
                     new_payload['addr'] = new_payload['road_address']
 
         yield new_payload
